be28c91a4a873e5b7831a351439934a1201b0bb7
hunglay <hunglay0420@gmail.com>
2018-10-02 00:25:55 -0700

    use CORS

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
-
+var cors = require('cors');
 // Create the application.
 var app = express();
 
@@ -16,12 +16,12 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost/playerconnect", { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/playerconnect');
 mongoose.connection.once('open', function() {
     app.models = require('./models/index');
 
@@ -30,33 +30,19 @@ mongoose.connection.once('open', function() {
     _.each(routes, function(controller, route) {
     app.use(route, controller(app, route));
     });
-<<<<<<< HEAD
-
-    console.log('Listening on port 3000...');
-    app.listen(3000);
-=======
 		const port = process.env.PORT || 9000;
     app.listen(port, function(){
       console.log(`listening on port ${port}...`);
     });
->>>>>>> 9a29f489d919fc58496b1d11d778f8d44955c4f4
-});
 
+});
 app.use('/hello', function(req, res, next) {
     res.send('Hello World!');
     next();
 });
 
+app.use(cors())
 
-app.get('/hello1', function(req, res){
-   res.send("Hello World!");
-});
-
-app.post('/hello1', function(req, res){
-   res.send("You just called the post method at '/hello'!\n");
-});
-
-var trys = require('./try.js');
-
-//both index.js and things.js should be in same directory
-app.use('/trys', trys);
+app.get('/products',  function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a Single Route'})
+})
