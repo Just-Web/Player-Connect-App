6e240e534c142db966b3c3536e45af76fab35615
sonamnahar21 <sonamnahar21@gmail.com>
2018-11-08 15:10:59 -0800

    added player component

--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
-import { PlayerComponent } from './player/player.component';
+import { PlayerComponent }      from './player/player.component';
 
 @NgModule({
   imports: [
