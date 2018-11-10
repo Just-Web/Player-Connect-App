167d5dcd4a01b4d91eb4fa2983fe49d587060d70
sonamnahar21 <sonamnahar21@gmail.com>
2018-11-09 18:03:24 -0800

    added about page

--- client2/src/app/app-routing.module.ts
+++ client2/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { PlayerComponent }  from './player/player.component';
+import { AboutComponent }  from './about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'player', component: PlayerComponent }
+  { path: 'player', component: PlayerComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
 import { PlayerComponent }      from './player/player.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports: [
@@ -37,7 +38,8 @@ import { PlayerComponent }      from './player/player.component';
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent,
-    PlayerComponent
+    PlayerComponent,
+    AboutComponent
   ],
   bootstrap: [ AppComponent ]
 })
