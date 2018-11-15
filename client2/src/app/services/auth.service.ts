64b76752f6ba8b6a2b9acc0319ff7a468ff81016
sonamnahar21 <sonamnahar21@gmail.com>
2018-11-15 13:42:06 -0800

    changed url back to localhost

--- client2/src/app/services/auth.service.ts
+++ client2/src/app/services/auth.service.ts
@@ -15,16 +15,13 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-      //use this while tesing on local host 3.16.119.157
-      // return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
-      //for production 
-      return this.http.post('http://3.16.119.157:3000/users/register', user,{headers: headers})
+      return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
       .pipe(map(res => res.json()));
   }
   authenticateUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://3.16.119.157:3000/users/authenticate', user,{headers: headers})
+    return this.http.post('http://localhost:3000/users/authenticate', user,{headers: headers})
       .pipe(map(res => res.json()));
   }
 }
