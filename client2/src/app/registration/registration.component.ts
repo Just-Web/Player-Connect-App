97a447e78f6d809179dce97090d9fc0423a75dae
sonamnahar21 <sonamnahar21@gmail.com>
2018-11-20 15:28:10 -0800

    completed registration end point integration

--- client2/src/app/registration/registration.component.ts
+++ client2/src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../services/validate.service';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+  socialsites: String;
+  games : String;
+  description: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
   }
+  onRegisterSubmit(){
+    const user={
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+      socialsites: this.socialsites,
+      games : this.games,
+      description: this.description
+    }
+    //Register User
+    this.authService.registerUser(user).subscribe(data=>{
+      if(data.success){
+        console.log('Registered success');
+        this.router.navigate(['/dashboard']);
+      } else{
+        console.log('Something went wrong');
+       // this.router.navigate(['/player_registration']);
+      }
+    });
 
+  }
 }
