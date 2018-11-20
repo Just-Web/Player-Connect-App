87cda7b6d596f1e4a393e474148b79c0fe73f561
sonamnahar21 <sonamnahar21@gmail.com>
2018-11-20 15:56:33 -0800

    logout not working

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {AuthService} from './services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
   title = 'Tour of Heroes';
+  onLogout(){
+    this.authService.logout().subscribe(data=>{
+      if(data.success){
+        console.log('Logout success');
+        this.router.navigate(['/registration']);
+      } else{
+        console.log('Something went wrong');
+      }
+    });
+  }
 }
